cc4276ee213cc19b58ee5b7f9c1df959081b303b
VittoriaBasile <vichy.basile@gmail.com>
2023-05-22 16:43:03 +0200

    class percorrenza e tratta

--- src/main/java/entities/Percorrenza.java
+++ src/main/java/entities/Percorrenza.java
@@ -1,5 +1,48 @@
 package entities;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "percorrenze")
+@Setter
+@Getter
+@NoArgsConstructor
 public class Percorrenza {
+	@Id
+	private UUID id = UUID.randomUUID();
+	private LocalDate oraPartenza;
+	private LocalDate oraArrivo;
+	private long tempoPercorrenza;
+	// @ManyToOne
+	private Veicolo veicoloId;
+	@ManyToOne
+	private Tratta trattaId;
+
+	public Percorrenza(LocalDate oraPartenza, LocalDate oraArrivo, Veicolo veicoloId, Tratta trattaId) {
+		this.oraPartenza = oraPartenza;
+		this.oraArrivo = oraArrivo;
+		this.veicoloId = veicoloId;
+		this.trattaId = trattaId;
+		this.tempoPercorrenza = ChronoUnit.SECONDS.between(oraArrivo, oraPartenza);
+
+	}
+
+	@Override
+	public String toString() {
+		return "Percorrenza [id=" + id + ", oraPartenza=" + oraPartenza + ", oraArrivo=" + oraArrivo
+				+ ", tempoPercorrenza=" + tempoPercorrenza + ", veicoloId=" + veicoloId + ", trattaId=" + trattaId
+				+ "]";
+	}
 
 }
--- src/main/java/entities/Tratta.java
+++ src/main/java/entities/Tratta.java
@@ -1,5 +1,38 @@
 package entities;
 
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tratte")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Tratta {
+	@Id
+	@OneToMany(mappedBy = "trattaId")
+	private UUID id = UUID.randomUUID();
+	private String partenza;
+	private String capolinea;
+	private double tempoPercorrenzaMedio;
+
+	public Tratta(String partenza, String capolinea) {
+		this.partenza = partenza;
+		this.capolinea = capolinea;
+	}
+
+	@Override
+	public String toString() {
+		return "Tratta [id=" + id + ", partenza=" + partenza + ", capolinea=" + capolinea + ", tempoPercorrenzaMedio="
+				+ tempoPercorrenzaMedio + "]";
+	}
 
 }
