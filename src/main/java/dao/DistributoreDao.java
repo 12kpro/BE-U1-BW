8e78242e4d88fd91b8443ded4f3081c4971bc1bd
VittoriaBasile <vichy.basile@gmail.com>
2023-05-23 10:50:19 +0200

    distributoreDao,UtenteDao,DocumentoViaggioDao basi

---
+++ src/main/java/dao/DistributoreDao.java
@@ -0,0 +1,50 @@
+package dao;
+
+import java.util.UUID;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.hibernate.HibernateException;
+import org.hibernate.exception.ConstraintViolationException;
+
+import entities.Distributore;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DistributoreDao {
+	private final EntityManager em;
+
+	public DistributoreDao(EntityManager em) {
+		this.em = em;
+	}
+
+	public void create(Distributore d) throws HibernateException, ConstraintViolationException {
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		em.persist(d);
+		transaction.commit();
+		log.info("Catalogo salvato!");
+
+	}
+
+	public Distributore getById(UUID id) throws HibernateException, ConstraintViolationException {
+
+		Distributore found = em.find(Distributore.class, id);
+		return found;
+	}
+
+	public void delete(UUID id) throws HibernateException, ConstraintViolationException {
+		Distributore found = em.find(Distributore.class, id);
+		if (found != null) {
+			EntityTransaction transaction = em.getTransaction();
+			transaction.begin();
+			em.remove(found);
+			transaction.commit();
+			log.info("Distributore con id " + id + " eliminato!");
+		} else {
+			log.info("Distributore con id " + id + " non trovato!");
+		}
+	}
+
+}
---
+++ src/main/java/dao/DocumentoViaggioDao.java
@@ -0,0 +1,50 @@
+package dao;
+
+import java.util.UUID;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.hibernate.HibernateException;
+import org.hibernate.exception.ConstraintViolationException;
+
+import entities.DocumentoViaggio;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DocumentoViaggioDao {
+	private final EntityManager em;
+
+	public DocumentoViaggioDao(EntityManager em) {
+		this.em = em;
+	}
+
+	public void create(DocumentoViaggio d) throws HibernateException, ConstraintViolationException {
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		em.persist(d);
+		transaction.commit();
+		log.info("DocumentoViaggio salvato!");
+
+	}
+
+	public DocumentoViaggio getById(UUID id) throws HibernateException, ConstraintViolationException {
+
+		DocumentoViaggio found = em.find(DocumentoViaggio.class, id);
+		return found;
+	}
+
+	public void delete(UUID id) throws HibernateException, ConstraintViolationException {
+		DocumentoViaggio found = em.find(DocumentoViaggio.class, id);
+		if (found != null) {
+			EntityTransaction transaction = em.getTransaction();
+			transaction.begin();
+			em.remove(found);
+			transaction.commit();
+			log.info("DocumentoViaggio con id " + id + " eliminato!");
+		} else {
+			log.info("DocumentoViaggio con id " + id + " non trovato!");
+		}
+	}
+
+}
