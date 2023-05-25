98f06fff86e33a054a4b692a48314240151f9e7f
12kpro <97319981+12kpro@users.noreply.github.com>
2023-05-25 18:01:42 +0200

    nativequery test

--- src/main/java/dao/PercorrenzaDAO.java
+++ src/main/java/dao/PercorrenzaDAO.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceException;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import entities.Percorrenza;
@@ -103,11 +104,19 @@ public class PercorrenzaDAO {
 		return getAllQuery.getResultList();
 	}
 
-	public List<Percorrenza> getTempoPercorrenzaPerVeicolo() throws PersistenceException {
-		TypedQuery<Percorrenza> getAllQuery = em.createQuery(
-				"SELECT trattaid_id ,veicoloid_id , SUM(EXTRACT(EPOCH FROM (oraarrivo - orapartenza)))  AS tempopercorrenza from Percorrenza p"
-						+ "WHERE oraarrivo IS NOT NULL" + "GROUP BY trattaid_id, veicoloid_id",
-				Percorrenza.class);
-		return getAllQuery.getResultList();
+	public void getTempoPercorrenzaPerVeicolo() throws PersistenceException {
+//		TypedQuery<Object[]> getAllQuery = em.createQuery(
+//				"SELECT p.tratta, p.veicolo, SUM(EXTRACT(EPOCH FROM (oraArrivo - oraPartenza)))  AS tempopercorrenza from Percorrenza p WHERE p.oraArrivo IS NOT NULL GROUP BY p.tratta, p.veicolo",
+//				Object[].class);
+//"select tratta_id ,veicolo_id , sum(EXTRACT(EPOCH FROM (oraarrivo - orapartenza)))  as percorrenza from percorrenze where oraarrivo is not null group by tratta_id, veicolo_id"
+
+		// List<Object[]> lista = getAllQuery.getResultList();
+		// log.info(lista.toString());
+		// return lista;
+		String sql = "select id, oraarrivo, orapartenza, tratta_id ,veicolo_id , sum(EXTRACT(EPOCH FROM (oraarrivo - orapartenza)))  as tempopercorrenza from percorrenze where oraarrivo is not null group by id, oraarrivo, orapartenza, tratta_id ,veicolo_id";
+		Query q = em.createNativeQuery(sql, Percorrenza.class);
+
+		log.info(q.getResultList().toString());
+
 	}
 }
--- src/main/java/entities/Percorrenza.java
+++ src/main/java/entities/Percorrenza.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import app.Application;
 import lombok.Getter;
@@ -29,10 +30,9 @@ public class Percorrenza {
 	@ManyToOne(optional = false)
 	private Veicolo veicolo;
 	@ManyToOne(optional = false)
-//	@Transient
-//	private int tempopercorrenza;
 	private Tratta tratta;
-
+	@Transient
+	private Double tempopercorrenza;
 
 	public Percorrenza(String oraPartenza, String oraArrivo, Veicolo veicolo, Tratta tratta) {
 		this.oraPartenza = LocalDateTime.parse(oraPartenza, Application.dateTimeFormatter);
@@ -42,6 +42,14 @@ public class Percorrenza {
 		/// this.tempoPercorrenza = ChronoUnit.SECONDS.between(this.oraArrivo,
 		/// this.oraPartenza);
 
+	}
+
+	public Percorrenza(String oraPartenza, String oraArrivo, Veicolo veicolo, Tratta tratta, Double tempopercorrenza) {
+		this.oraPartenza = LocalDateTime.parse(oraPartenza, Application.dateTimeFormatter);
+		this.oraArrivo = LocalDateTime.parse(oraArrivo, Application.dateTimeFormatter);
+		this.veicolo = veicolo;
+		this.tratta = tratta;
+		this.tempopercorrenza = tempopercorrenza;
 	}
 
 	@Override
