3604f1d05af07fd5bbca6672cf010832a03340b2
VittoriaBasile <vichy.basile@gmail.com>
2023-05-26 14:36:41 +0200

    metodi prompt giusti

--- src/main/java/app/Prompt.java
+++ src/main/java/app/Prompt.java
@@ -1,6 +1,8 @@
 package app;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -53,11 +55,10 @@ public class Prompt {
 
 	public static void main(String[] args) {
 
-		String options[] = { "0: Per Uscire", "1: Esegui ricerche", "2: Visualizza tempo di percorrenza",
-				"3: Visualizza tempi di percorrenza medi per tratta", "4: Visualizza Abbonamenti Scaduti",
-				"5: Tessere scadute", "6: Biglietti vidimati in un dato periodo",
-				"7: Documenti viaggio per data e distributore", "8: Biglietti vidimati per veicolo",
-				"9 Percorrenza tratte per veicolo", "10: Carica dati di esempio" };
+		String options[] = { "0: Per Uscire", "1: Tempo medio per tratta", "2: Visualizza tempo di percorrenza",
+				"3: FindAll", "4: Visualizza Abbonamenti Scaduti", "5: Tessere scadute",
+				"6: Biglietti vidimati in un dato periodo", "7: Documenti viaggio per data e distributore",
+				"8: Biglietti vidimati per veicolo", "9 Percorrenza tratte per veicolo", "10: Carica dati di esempio" };
 
 		choice: while (true) {
 			try {
@@ -69,7 +70,7 @@ public class Prompt {
 					input.close();
 					break choice;
 				case 1:
-					ricerche();
+					// ricerche();
 					break;
 				case 2:
 					for (Percorrenza result : pd.getTempoPercorrenzaPerVeicolo()) {
@@ -116,6 +117,9 @@ public class Prompt {
 				log.info("Devi inserire un numero intero positivo!!");
 			} catch (PersistenceException e) {
 				log.error("Errore durante la lettura/scrittura {}", e.getMessage());
+			} catch (DateTimeParseException e) {
+				log.info("Devi inserire la data nel formato 'dd-mm-yyyy'!");
+
 			}
 			// em.close();
 		}
@@ -178,21 +182,16 @@ public class Prompt {
 		}
 	}
 
-	public static void bigliettiVidimatiInUnDatoPeriodo() {
+	public static void bigliettiVidimatiInUnDatoPeriodo() throws DateTimeParseException {
+
 		System.out.println("Seleziona una data di inizio nel formato 'dd-mm-yyyy'");
 		String dataInizio = input.nextLine();
-		/*
-		 * if (DateTimeFormatter.ofPattern(dataInizio) != dateFormatter) {
-		 * log.info("il formato della data inserita non è valido");
-		 * 
-		 * }
-		 */
+		LocalDate.parse(dataInizio, dateFormatter);
+
 		System.out.println("Seleziona una data di fine nel formato 'dd-mm-yyyy'");
 		String dataFine = input.nextLine();
-		/*
-		 * if (DateTimeFormatter.ofPattern(dataFine) != dateFormatter) {
-		 * log.info("il formato della data inserita non è valido"); }
-		 */
+		LocalDate.parse(dataFine, dateFormatter);
+
 		List<Biglietto> numBiglietti = dvd.getBigliettiVidimatiPerPeriodo(dataInizio, dataFine);
 
 		if (numBiglietti.size() > 0) {
@@ -205,21 +204,13 @@ public class Prompt {
 		}
 	}
 
-	public static void documentiViaggioPerDataEDistributore() {
+	public static void documentiViaggioPerDataEDistributore() throws DateTimeParseException {
 		System.out.println("Seleziona una data di inizio nel formato 'dd-mm-yyyy'");
 		String dataInizio = input.nextLine();
-		/*
-		 * if (DateTimeFormatter.ofPattern(dataInizio) != dateFormatter) {
-		 * log.info("il formato della data inserita non è valido");
-		 * 
-		 * }
-		 */
+		LocalDate.parse(dataInizio, dateFormatter);
 		System.out.println("Seleziona una data di fine nel formato 'dd-mm-yyyy'");
 		String dataFine = input.nextLine();
-		/*
-		 * if (DateTimeFormatter.ofPattern(dataFine) != dateFormatter) {
-		 * log.info("il formato della data inserita non è valido"); }
-		 */
+		LocalDate.parse(dataFine, dateFormatter);
 		List<Distributore> distributori = dd.findAll();
 
 		while (true) {
@@ -355,39 +346,4 @@ public class Prompt {
 		pd.createByList(percorrenze);
 	}
 
-	public static void ricerche() {
-		String ricercheOpt[] = { "0: Esci ", "1: Visualizza tutte le tessere scadute",
-				"2 Visualizza tutte le tessere scadute per data" };
-//		System.out.print("Inserisci ISBN:");
-//		Long codice = Math.abs(Long.parseLong(input.nextLine()));
-//		System.out.print("Inserisci titolo:");
-//		String titolo = input.nextLine();
-//		System.out.print("Inserisci Anno Pubblicazione:");
-//		Integer annoPubblicazione = Math.abs(Integer.parseInt(input.nextLine()));
-//		System.out.print("Inserisci Numero Pagine:");
-//		Integer numPagine = Math.abs(Integer.parseInt(input.nextLine()));
-//		System.out.print("Inserisci Periodicità:");
-		ricercheLoop: while (true) {
-			System.out.println(Arrays.asList(ricercheOpt));
-			int ricercheSel = Math.abs(Integer.parseInt(input.nextLine()));
-
-			switch (ricercheSel) {
-			case 0:
-				break ricercheLoop;
-			case 1:
-				for (Utente result : ud.findExpiredNow()) {
-					log.info(result.toString());
-				}
-				break;
-			case 2:
-				for (Utente result : ud.findExpiredByDate(input.nextLine())) {
-					log.info(result.toString());
-				}
-				break;
-			default:
-				log.info("L'opzione inserita " + ricercheSel + " non è valida!");
-				break;
-			}
-		}
-	}
 }
